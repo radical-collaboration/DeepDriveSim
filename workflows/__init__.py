d9e4d413991f975baa4d8fcb673d53c47c9432e3
Mariya Goliyad <goliyad@br014.ib.bridges2.psc.edu>
2026-03-06 18:11:46 -0500

    leftover from prev commit

--- workflows/__init__.py
+++ workflows/__init__.py
@@ -25,11 +25,17 @@ DummyWorkflow = _optional_import(
 MiniAppsWorkflow = _optional_import(
     "workflows.miniapps_workflow.miniapps_workflow", "MiniAppsWorkflow"
 )
-
+MiniAppsWorkflow = _optional_import(
+    "workflows.miniapps_workflow.miniapps_workflow", "MiniAppsWorkflow"
+)
+AdaptiveWorkflow = _optional_import(
+    "workflows.adaptive_ensemble.adaptive_workflow", "AdaptiveWorkflow"
+)
 __version__ = "0.1.0"
 
 __all__ = [
     "DDMdWorkflow",
     "DummyWorkflow",
     "MiniAppsWorkflow",
+    "AdaptiveWorkflow",
 ]
